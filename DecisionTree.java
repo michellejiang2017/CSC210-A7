de0ce9dc49dfb1ab4d06b5fe52177e9ee275b850
Michelle Jiang <mhjiang@smith.edu>
2026-04-02 15:59:23 -0400

    phase 1 tests

--- DecisionTree.java
+++ DecisionTree.java
@@ -18,20 +18,73 @@ public class DecisionTree extends BinaryTree<String> {
         super(data, left, right);
     }
 
+    /**
+     * 
+     * @param data
+     * @return
+     */
     public DecisionTree followPath(String data) { 
         DecisionTree current = this; 
         for (int i = 0; i < data.length(); i++) { 
             char c = data.charAt(i);
-            if (current == null) { 
-                return null; 
-            } else if (c == 'Y') { 
+            if (c == 'Y') { 
                 current = (DecisionTree) current.getLeft();  
             } else if (c == 'N') { 
                 current = (DecisionTree) current.getRight(); 
+            } else { 
+                return null; 
             }
         }
         return current;
     }
 
+    public static void main(String[] args) {
+
+    // Leaves (animals)
+    DecisionTree dog = new DecisionTree("Dog");
+    DecisionTree mouse = new DecisionTree("Mouse");
+    DecisionTree crocodile = new DecisionTree("Crocodile");
+
+    // Intermediate node
+    DecisionTree cat = new DecisionTree("Cat", mouse, null);
+
+    // Question node
+    DecisionTree bark = new DecisionTree("Does it bark?", dog, cat);
+
+    // Root
+    DecisionTree root = new DecisionTree("Is it a mammal?", bark, crocodile);
+
+    // ---- TESTS ----
+
+    // Direct navigation
+    System.out.println(root.getData()); // Is it a mammal?
+
+    System.out.println(root.getLeft().getData()); // Does it bark?
+    System.out.println(root.getRight().getData()); // Crocodile
+
+    System.out.println(root.getLeft().getLeft().getData()); // Dog
+    System.out.println(root.getLeft().getRight().getData()); // Cat
+
+    System.out.println(root.getLeft().getRight().getLeft().getData()); // Mouse
+
+    // ---- followPath tests ----
+    System.out.println(root.followPath("").getData());        // root → Is it a mammal?
+    System.out.println(root.followPath("Y").getData());       // Does it bark?
+    System.out.println(root.followPath("N").getData());       // Crocodile
+    System.out.println(root.followPath("YY").getData());      // Dog
+    System.out.println(root.followPath("YN").getData());      // Cat
+    try { 
+        System.out.println(root.followPath("YNL").getData());     // should break (invalid char)
+    } catch (Exception e) { 
+        System.out.println("Exception");
+    }
+    try { 
+        System.out.println(root.followPath("YNY"));               // likely null (no right child)
+    } catch (Exception e) { 
+        System.out.println("Exception");
+    }
+    
+
+}
 
 }
